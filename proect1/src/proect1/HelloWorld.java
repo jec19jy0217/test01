c79722dd23f3fdaa7218ddded2061e7fc0ac36bf
19jy0217 <19jy0217@PC39.jynet2.jec.ac.jp>
2020-11-12 09:58:16 +0900

    文の追加

--- proect1/src/proect1/HelloWorld.java
+++ proect1/src/proect1/HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
 	}
 
 }
